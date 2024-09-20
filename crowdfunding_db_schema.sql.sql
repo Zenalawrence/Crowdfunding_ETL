def242882ac52fe763baf381875e9a74209b9030
Zenalawrence <148465597+Zenalawrence@users.noreply.github.com>
2024-09-19 20:36:30 -0400

    Add files via upload

---
+++ crowdfunding_db_schema.sql.sql
@@ -0,0 +1,38 @@
+-- Creating Tables
+CREATE TABLE category (
+    category_id VARCHAR PRIMARY KEY,
+    category VARCHAR
+);
+
+CREATE TABLE category (
+    subcategory_id VARCHAR PRIMARY KEY,
+    subcategory VARCHAR
+);
+
+CREATE TABLE contacts (
+    contact_id INT PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR
+);
+
+
+CREATE TABLE campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id) ,
+    company_name VARCHAR,
+    description VARCHAR,
+    goal FLOAT,
+    pledged FLOAT,
+    outcome VARCHAR,
+    backers_count INT,
+    country VARCHAR,
+    currency VARCHAR,
+    launched_date VARCHAR,
+    end_date VARCHAR,
+    category_id VARCHAR,
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    subcategory_id VARCHAR,
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
